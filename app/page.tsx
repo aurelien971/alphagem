2fe5b54d1b7af2a0f60cb25c21e0796af4944503
Aurelien Nicolle <aureliennicolle@Aureliens-Laptop.local>
2026-01-16 21:23:05 +0000

    Remove broken SustainableCommitmentSection import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/home/HeroSection";
 import VisionRoleSection from "@/components/home/VisionRoleSection";
-import SustainableCommitmentSection from "@/components/home/SustainableCommitmentSection";
 
 export default function HomePage() {
   return (
